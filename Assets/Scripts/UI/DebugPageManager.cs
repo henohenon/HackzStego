5c77c618fb61de6d1195d5d1752fdd87b35e87d4
henohenon <takeru.hawk.son@gmail.com>
2024-06-15 20:16:45 +0900

    set scene props

--- Assets/Scripts/UI/DebugPageManager.cs
+++ Assets/Scripts/UI/DebugPageManager.cs
@@ -4,18 +4,19 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using R3;
+using TMPro;
 
 public class DebugPageManager : MonoBehaviour
 {
     [SerializeField] private LightEstimate _lightEstimate;
     [SerializeField] private SoundManager _soundManager;
     [SerializeField] private ScoringManager _scoringManager;
-    [SerializeField] private Text brightLevel;
-    [SerializeField] private Text temperatureLevel;
-    [SerializeField] private Text volumeLevel;
-    [SerializeField] private Text brightValue;
-    [SerializeField] private Text temperatureValue;
-    [SerializeField] private Text volumeValue;
+    [SerializeField] private TMP_Text brightLevel;
+    [SerializeField] private TMP_Text temperatureLevel;
+    [SerializeField] private TMP_Text volumeLevel;
+    [SerializeField] private TMP_Text brightValue;
+    [SerializeField] private TMP_Text temperatureValue;
+    [SerializeField] private TMP_Text volumeValue;
 
     
     private void Start()
